b19506af800992ba44c3ca39b9593140337c6e7b
Francesco Lamberti <francesco.lamberti@gmail.com>
2023-05-16 11:24:06 +0200

    feat: added a slot to handle incoming message carrying DeviceData information

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -2,51 +2,46 @@
 
 #include <QPointer>
 
-Controller *Controller::instance()
-{
-    static QPointer<Controller> _instance;
-    if(!_instance){
-        _instance = new Controller;
-    }
-    return _instance;
+Controller *Controller::instance() {
+  static QPointer<Controller> _instance;
+  if (!_instance) {
+    _instance = new Controller;
+  }
+  return _instance;
 }
 
+std::shared_ptr<DeviceListModel> Controller::model() const { return m_model; }
 
-std::shared_ptr<DeviceListModel> Controller::model() const
-{
-    return m_model;
+void Controller::set_model(const std::shared_ptr<DeviceListModel> &model) {
+  if (m_model != model) {
+    return;
+  }
+  m_model = model;
+  emit model_changed();
 }
 
-void Controller::set_model(const std::shared_ptr<DeviceListModel> &model)
-{
-    if(m_model != model){
-        return;
-    }
-    m_model = model;
-    emit model_changed();
+Controller::Controller(QObject *parent) : QObject(parent) {
+  m_model = std::make_shared<DeviceListModel>(this);
+  m_filter_model =
+      std::make_shared<DeviceFilterProxyModel>(m_model.get(), this);
 }
 
-Controller::Controller(QObject *parent)
-    : QObject(parent)
-{
-    m_model = std::make_shared<DeviceListModel>(this);
-    m_filter_model = std::make_shared<DeviceFilterProxyModel>(m_model.get(), this);
+DeviceFilterProxyModel *Controller::model_data() const {
+  return m_filter_model.get();
 }
 
-DeviceFilterProxyModel *Controller::model_data() const
-{
-    return m_filter_model.get();
+std::shared_ptr<DeviceFilterProxyModel> Controller::filter_model() const {
+  return m_filter_model;
 }
 
-std::shared_ptr<DeviceFilterProxyModel> Controller::filter_model() const
-{
-    return m_filter_model;
+void Controller::set_filter_model(
+    const std::shared_ptr<DeviceFilterProxyModel> &filter_model) {
+  if (m_filter_model == filter_model)
+    return;
+  m_filter_model = filter_model;
+  emit filter_model_changed();
 }
 
-void Controller::set_filter_model(const std::shared_ptr<DeviceFilterProxyModel> &filter_model)
-{
-    if (m_filter_model == filter_model)
-        return;
-    m_filter_model = filter_model;
-    emit filter_model_changed();
+void Controller::register_device_information(const DeviceData &deviceData) {
+  m_model->add_device(deviceData);
 }
--- src/Controller.h
+++ src/Controller.h
@@ -1,38 +1,44 @@
 #pragma once
 
-#include "DeviceListModel.h"
 #include "DeviceFilterProxyModel.h"
-#include <memory>
+#include "DeviceListModel.h"
 #include <QDate>
 #include <QObject>
+#include <memory>
 
-class Controller : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(DeviceFilterProxyModel* filter_model READ model_data NOTIFY filter_model_changed)
+class Controller : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(DeviceFilterProxyModel *filter_model READ model_data NOTIFY
+                 filter_model_changed)
 
 public:
-    static Controller *instance();
+  static Controller *instance();
 
-    [[nodiscard]] std::shared_ptr<DeviceListModel> model() const;
-    void set_model(std::shared_ptr<DeviceListModel> const& model);
+  [[nodiscard]] std::shared_ptr<DeviceListModel> model() const;
+  void set_model(std::shared_ptr<DeviceListModel> const &model);
 
-    [[nodiscard]] std::shared_ptr<DeviceFilterProxyModel> filter_model() const;
-    void set_filter_model(const std::shared_ptr<DeviceFilterProxyModel> &filter_model);
+  [[nodiscard]] std::shared_ptr<DeviceFilterProxyModel> filter_model() const;
+  void
+  set_filter_model(const std::shared_ptr<DeviceFilterProxyModel> &filter_model);
+
+  /// \brief handle_device_data includes the new incoming information contained
+  /// \a deviceData to the underlying model \param deviceData information about
+  /// an already known device or about a newly discovered device
+  void register_device_information(const DeviceData &deviceData);
 
 signals:
-    void model_changed();
-    void filter_model_changed();
-    void devices_changed();
+  void model_changed();
+  void filter_model_changed();
+  void devices_changed();
 
 private:
-    Controller(QObject *parent = nullptr);
+  Controller(QObject *parent = nullptr);
 
-    DeviceFilterProxyModel *model_data() const;
+  DeviceFilterProxyModel *model_data() const;
 
-    std::shared_ptr<DeviceListModel> m_model;
-    std::shared_ptr<DeviceFilterProxyModel> m_filter_model;
-    Controller * m_controller = nullptr;
+  std::shared_ptr<DeviceListModel> m_model;
+  std::shared_ptr<DeviceFilterProxyModel> m_filter_model;
+  Controller *m_controller = nullptr;
 };
 
-Q_DECLARE_METATYPE(Controller*);
+Q_DECLARE_METATYPE(Controller *);
